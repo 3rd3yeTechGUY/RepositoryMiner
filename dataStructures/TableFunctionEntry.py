a20a3aad9adc72f34439ecdf292d58eefae0eba3
tobiasfamos <tfamos@gmx.ch>
2020-03-05 12:18:45 +0100

    Construct table functon entry from method dto and metrics

--- dataStructures/TableFunctionEntry.py
+++ dataStructures/TableFunctionEntry.py
@@ -2,35 +2,36 @@ from Utils.CSV import build_csv_from_array
 
 
 class TableFunctionEntry:
-    def __init__(self, commit, modification, method, stmt_count, author_metrics, decl_metrics, condition_metrics
-                 ):
-        self.hash = commit.hash
-        self.method_long_name = method.long_name
-        self.file_name = modification.filename
-        self.fan_in = method.fan_in
-        self.fan_out = method.fan_out
-        self.local_variables = method.nloc
-        self.parameters = len(method.parameters)
-        self.comment_to_code_ratio = method.comment_to_code_ration
-        self.count_path = method.path_count
-        self.complexity = method.complexity
-        self.exec_statements = method.exec_statements
-        self.max_nesting = method.top_nesting_level
-        self.method_histories = stmt_count["method_histories"]
-        self.authors = author_metrics
-        self.stmt_added = stmt_count["sum_statement_added"]
-        self.max_stmt_added = stmt_count["max_statement_added"]
-        self.avg_stmt_added = stmt_count["average_statement_added"]
-        self.stmt_deleted = stmt_count["sum_statement_deleted"]
-        self.max_stmt_deleted = stmt_count["max_statement_deleted"]
-        self.avg_stmt_deleted = stmt_count["average_statement_deleted"]
-        self.churn = stmt_count["churn"]
-        self.max_churn = stmt_count["max_churn"]
-        self.avg_churn = stmt_count["average_churn"]
-        self.decl = decl_metrics
-        self.cond = condition_metrics["condition_changes"]
-        self.else_added = condition_metrics["else_added"]
-        self.else_deleted = condition_metrics["else_removed"]
+    def __init__(self, method_dto, metrics):
+        self.hash = method_dto.commit.hash
+        self.method_long_name = method_dto.method_long_name
+        self.file_name = method_dto.modified_file.filename
+        self.sum_statement_added = metrics["sum_statement_added"]
+
+        # self.fan_in = method.fan_in
+        # self.fan_out = method.fan_out
+        # self.local_variables = method.nloc
+        # self.parameters = len(method.parameters)
+        # self.comment_to_code_ratio = method.comment_to_code_ration
+        # self.count_path = method.path_count
+        # self.complexity = method.complexity
+        # self.exec_statements = method.exec_statements
+        # self.max_nesting = method.top_nesting_level
+        # self.method_histories = stmt_count["method_histories"]
+        # self.authors = author_metrics
+        # self.stmt_added = stmt_count["sum_statement_added"]
+        # self.max_stmt_added = stmt_count["max_statement_added"]
+        # self.avg_stmt_added = stmt_count["average_statement_added"]
+        # self.stmt_deleted = stmt_count["sum_statement_deleted"]
+        # self.max_stmt_deleted = stmt_count["max_statement_deleted"]
+        # self.avg_stmt_deleted = stmt_count["average_statement_deleted"]
+        # self.churn = stmt_count["churn"]
+        # self.max_churn = stmt_count["max_churn"]
+        # self.avg_churn = stmt_count["average_churn"]
+        # self.decl = decl_metrics
+        # self.cond = condition_metrics["condition_changes"]
+        # self.else_added = condition_metrics["else_added"]
+        # self.else_deleted = condition_metrics["else_removed"]
 
     def get_as_csv(self):
         return build_csv_from_array(self.get_array_of_metrics())
